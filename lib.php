560c9e0ddc8af08a265730d669d56c39d6d2ff8a
Andrew Hancox <andrewdchancox@googlemail.com>
2022-02-23 12:59:02 +0000

    Queue up JS calls to run off callback to allow for filters being run prior to URL or context being set - fix

--- lib.php
+++ lib.php
@@ -93,9 +93,10 @@ function filter_translations_before_footer() {
     if (empty(\filter_translations::$translationstoinject)) {
         return;
     }
-
-    $PAGE->requires->js_call_amd('filter_translations/translation_button', 'init', ['returnurl' => $PAGE->url->out()]);
+    
     foreach (\filter_translations::$translationstoinject as $id => $jsobj) {
         $PAGE->requires->js_amd_inline("require(['filter_translations/translation_button'], function(translation_button) { translation_button.register('$id', $jsobj);});");
     }
+
+    $PAGE->requires->js_call_amd('filter_translations/translation_button', 'init', ['returnurl' => $PAGE->url->out()]);
 }
